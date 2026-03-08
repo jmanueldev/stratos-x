ed937f24b2ac4b0eed2b6d11857e39dad700085a
jmanueldev <62441933+jmanueldev@users.noreply.github.com>
2026-03-08 13:22:59 -0400

    Add telemetry handler and data structure

--- api/handlers /telemetry.go
+++ api/handlers /telemetry.go
@@ -1,1 +1,21 @@
+package handlers
 
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type Telemetry struct {
+	NodeID string  `json:"node_id"`
+	CPU    float64 `json:"cpu"`
+	Memory float64 `json:"memory"`
+}
+
+var TelemetryData = []Telemetry{
+	{"node-1", 12.5, 2048},
+	{"node-2", 45.2, 4096},
+}
+
+func ListTelemetry(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(TelemetryData)
+}
