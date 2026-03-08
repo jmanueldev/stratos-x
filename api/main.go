6b1b7212f537f4c1194d99ad967cdc3422058525
jmanueldev <62441933+jmanueldev@users.noreply.github.com>
2026-03-08 13:22:14 -0400

    Set up basic API routes in main.go

--- api/main.go
+++ api/main.go
@@ -1,1 +1,17 @@
+package main
 
+import (
+	"net/http"
+	"log"
+	"stratos-x/api/handlers"
+)
+
+func main() {
+	http.HandleFunc("/missions", handlers.SubmitMission)
+	http.HandleFunc("/missions/list", handlers.ListMissions)
+	http.HandleFunc("/nodes/list", handlers.ListNodes)
+	http.HandleFunc("/telemetry", handlers.ListTelemetry)
+
+	log.Println("STRATOS-X API running on :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
