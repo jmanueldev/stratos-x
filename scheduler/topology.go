1bc19a7784f33345b2232ea49faff8fc5b9ba428
jmanueldev <62441933+jmanueldev@users.noreply.github.com>
2026-03-08 13:23:33 -0400

    Implement MeshTopology function in topology.go

--- scheduler/topology.go
+++ scheduler/topology.go
@@ -1,1 +1,19 @@
+package scheduler
 
+type Topology struct {
+	Name        string
+	Connections map[string][]string
+}
+
+func MeshTopology(nodes []Node) Topology {
+	conn := make(map[string][]string)
+	for i := range nodes {
+		if i+1 < len(nodes) {
+			conn[nodes[i].ID] = append(conn[nodes[i].ID], nodes[i+1].ID)
+		}
+		if i-1 >= 0 {
+			conn[nodes[i].ID] = append(conn[nodes[i].ID], nodes[i-1].ID)
+		}
+	}
+	return Topology{Name: "mesh", Connections: conn}
+}
