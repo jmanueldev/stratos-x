220fa40c9cce1eaaa9211da2ffeef770785c4498
jmanueldev <62441933+jmanueldev@users.noreply.github.com>
2026-03-08 13:40:07 -0400

    Add Gossip function for sending telemetry data

--- telemetry/gossip.go
+++ telemetry/gossip.go
@@ -1,1 +1,25 @@
+package telemetry
 
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+type Telemetry struct {
+	NodeID string
+	CPU float64
+	Memory float64
+}
+
+func Gossip(peers []string, data Telemetry) {
+
+	rand.Seed(time.Now().Unix())
+
+	for i:=0;i<3;i++{
+
+		p := peers[rand.Intn(len(peers))]
+
+		fmt.Println("sending telemetry to",p,data)
+	}
+}
