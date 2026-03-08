aeda082c727efc4c2e377b47f901c370b6b2c73e
jmanueldev <62441933+jmanueldev@users.noreply.github.com>
2026-03-08 13:25:21 -0400

    Add Checkpoint struct and save/recover functions

--- storage/checkpoint.go
+++ storage/checkpoint.go
@@ -1,1 +1,23 @@
+package storage
 
+import (
+	"fmt"
+	"time"
+)
+
+type Checkpoint struct {
+	MissionID string
+	NodeID    string
+	Timestamp time.Time
+	Data      string
+}
+
+func SaveCheckpoint(missionID, nodeID, data string) Checkpoint {
+	cp := Checkpoint{MissionID: missionID, NodeID: nodeID, Timestamp: time.Now(), Data: data}
+	fmt.Printf("Checkpoint saved for %s at node %s\n", missionID, nodeID)
+	return cp
+}
+
+func RecoverCheckpoint(cp Checkpoint) {
+	fmt.Printf("Recovered checkpoint for %s at node %s\n", cp.MissionID, cp.NodeID)
+}
