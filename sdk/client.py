5f0ca5cb72d13e6d10595eebd1d5a096259ffb6f
jmanueldev <62441933+jmanueldev@users.noreply.github.com>
2026-03-08 13:32:53 -0400

    Add functions to submit and list missions

--- sdk/client.py
+++ sdk/client.py
@@ -1,1 +1,14 @@
+import requests
+import yaml
 
+API = "http://localhost:8080"
+
+def submit_mission(file):
+    with open(file) as f:
+        data = yaml.safe_load(f)
+    r = requests.post(f"{API}/missions", json=data)
+    print(r.json())
+
+def list_missions():
+    r = requests.get(f"{API}/missions/list")
+    print(r.json())
