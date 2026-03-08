efd370f2970ef5ced7011bb40bc52eedc4808173
jmanueldev <62441933+jmanueldev@users.noreply.github.com>
2026-03-08 13:40:36 -0400

    Add Dataset struct and StageDataset function

--- storage/dataset_manager.go
+++ storage/dataset_manager.go
@@ -1,1 +1,16 @@
+package storage
 
+import "fmt"
+
+type Dataset struct {
+	Name string
+	Location string
+}
+
+func StageDataset(d Dataset, nodes []string){
+
+	for _,n := range nodes {
+
+		fmt.Println("staging dataset",d.Name,"to",n)
+	}
+}
